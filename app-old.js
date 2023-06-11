18b863a9dc7a389c00057782edb7e4700e602bcd
ignacio Castillo <Ignacio.cas.1992@gmail.com>
2023-06-11 04:37:13 -0400

    Webserver listo

---
+++ app-old.js
@@ -0,0 +1,18 @@
+
+
+
+const http = require('http');
+
+http.createServer( (req, res)=>{
+
+    //res.writeHead(200,{'Content-Type': 'text/plain'});
+    //res.writeHead(200,{'Content-Type': 'application/csv'});
+
+    res.write( 'hola mundo' );
+
+    res.end();
+
+})
+.listen( 8080 );
+
+console.log('escuchando el puerto', 8080);
---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+// handlebar
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estatico
+app.use( express.static('public'));
+
+app.get('/home',  (req, res) => {
+  res.render('home', {
+    nombre: ' Ignacio Castillo ',
+    titulo:' Curso de Node '
+  });
+})
+
+app.get('/generic',  (req, res) => {
+  res.render('generic', {
+    
+  });
+})
+
+app.get('/elements',  (req, res) => {
+  res.render('elements', {
+    
+  });
+})
+
+app.get('/generic',  (req, res) => {
+    res.sendFile( __dirname + '/public/generic.html')
+})
+
+app.get('/elements',  (req, res) =>{
+  res.sendFile( __dirname + '/public/elements.html') 
+ })
+
+app.get('*',  (req, res) => {
+    res.sendFile( __dirname + '/public/404.html')
+  })
+
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
